b9962b38d1c87d8db07e03b6e751af8555728e0c
Murat Hoca <mbshurricane@gmail.com>
2026-01-11 22:55:08 +0300

    mqtt
    mqtt

---
+++ LOLiN_MQTTprogram_v4/fbokuyaz.ino
@@ -0,0 +1,392 @@
+int fbsayac=0;
+String esayacyani="";
+void fbsayacoku()
+{
+  if(bestursay>0)
+  {
+    bestursay+=1;
+    if(bestursay>2)bestursay=0;
+  }
+if(fbpinayaryaz==true){fbpinayaryaz=false;fbpinayarlariyaz();}
+
+                      String getpath="/" + YOL + "/r/"+esphostname;
+//                    String resul=Firebase.RTDB.getString(&fbdo, getpath) ? fbdo.to<const char *>() : fbdo.errorReason().c_str();
+                      String resul = Database.get<String>(aClient, getpath);
+Serial.print(getpath +"  ");Serial.println(WiFi.localIP());
+Serial.println("Get str." + resul);
+
+                          if(resul.indexOf("path not exist")>-1 || resul=="null")
+                          {
+                            Serial.println("path not exist");
+                            fbdataguncelle();
+                          }
+                          else
+                          {
+                            if(resul.indexOf("|")>-1){
+                              String rsltt=resul.substring(resul.indexOf("|")+1,resul.length());
+                              resul=resul.substring(0,resul.indexOf("|"));
+                              fbsayacyanioku(rsltt);
+                            }
+                            if(resul.toInt()==9 || resul.toInt()==19)fbpinstatelerioku();
+                            
+                            if(resul.toInt()==10){resul="-1";fbpinstateleriyaz();}
+                              else                         
+                                if(psco==true || psci==true) fbpinstateleriyaz();
+
+                          }
+  yield();
+}
+
+void fbsayacyanioku(String rsltt)
+{ if(esayacyani!=rsltt){bestursay=1;esayacyani=rsltt;}
+
+String rslttmp = rsltt;
+Serial.println(rslttmp);
+                            for(int k=0;k<5;k++){
+                              String pnm;
+                              String pns;
+                                  if(rslttmp.indexOf(":")>-1)pnm=rslttmp.substring(0,rslttmp.indexOf(":"));
+                                  if(rslttmp.indexOf(",")>-1){
+                                    pns=rslttmp.substring(rslttmp.indexOf(":")+1,rslttmp.indexOf(","));
+                                    rslttmp = rslttmp.substring(rslttmp.indexOf(",")+1,rslttmp.length());
+                                  }
+                                  else
+                                  {
+                                    pns=rslttmp.substring(rslttmp.indexOf(":")+1,rslttmp.length());
+                                    rslttmp = "";
+                                  }
+                                  
+
+Serial.print("pnm:");Serial.print(pnm);Serial.print(" pns:");Serial.print(pns);
+
+                                if(pnm.indexOf("VR")==0)
+                                {
+                                Serial.println(pnm+" "+pns);
+                                  for(int vrr=0;vrr<6;vrr++)
+                                  {
+                                    if(pnm.indexOf("VR"+(String)vrr)==0)VRP[vrr]=pns;
+                                  }
+                                }
+                                else
+                                for(int hh=0;hh<11;hh++)
+                                {
+                                      if(pnm == pinname[hh])
+                                      { 
+                                            if(psco==false && pns!=fbPinState[hh])
+                                            {
+                                              PinState[hh] =pns;
+                                              //ePinState[hh] = pns;
+                                              fbPinState[hh] = pns;
+                                              //pindurumrecyap=true;
+                                            }
+                                        break;
+                                      }
+                                }
+                                if(rslttmp.length()<2)break;
+                            }
+}
+
+
+void fbbaskacihazagonder(String yol2,String fbdt,int fbg)
+{
+
+                      //Serial.print("yol2: ");Serial.println(yol2);
+                      //Serial.print("fbdt: ");Serial.println(fbdt);
+                      Serial.print("fbg: ");Serial.println(fbg);
+                      
+                      String getpath2=yol2;
+//                    String resul=Firebase.RTDB.getString(&fbdo, getpath2) ? fbdo.to<const char *>() : fbdo.errorReason().c_str();
+                      String resul = Database.get<String>(aClient, getpath2);
+                      Serial.println(getpath2 +"  ");
+                      Serial.println("Get str." + resul);
+                          if(resul.indexOf("path not exist")>-1 || resul=="null")
+                          {
+                                
+                          }
+                          else{
+                            String setpath2=yol2;
+                            String dats2=fbdt;
+                            int sayac=resul.toInt();
+                            if(sayac==9)sayac=19; else sayac=10;
+                            dats2=String(sayac) + "|" + fbdt;
+                            Serial.println("DAAATS: " + dats2);
+                            bool statusz =Database.set<string_t>(aClient, setpath2, string_t(dats2));
+                            if (statusz)
+                                {Serial.println("ok");
+                                efbtd[fbg]=fbtd[fbg];
+                                }
+                            else
+                                Firebase.printf("Error, msg: %s, code: %d\n", aClient.lastError().message().c_str(), aClient.lastError().code());
+                                fbtd[fbg]=efbtd[fbg];
+                          }
+}
+
+void fbsayacguncelle()
+{  if(bestursay!=0)return;
+                            String setpath="/" + YOL + "/r/"+esphostname;
+                            fbsayac+=1;
+                            if(fbsayac>7)fbsayac=0;
+//                            String resul=Firebase.RTDB.setString(&fbdo, setpath, F("0")) ? (String)fbsayac : fbdo.errorReason().c_str();
+                            Database.set<string_t>(aClient, setpath, string_t((String)fbsayac));
+}
+
+
+void fbdataguncelle()
+{
+Serial.println("fbdataguncelle------------");
+
+                              //sayac zaman guncelle 0 yap
+                                  fbsayacguncelle();
+                              ////////////////////////////
+
+                              // pin ayarlarını yaz
+                                  fbpinayarlariyaz();
+                              /////////////////////////////
+
+                              //pinstateleri yaz
+                                  fbpinstateleriyaz();
+                              /////////////////////////////
+}
+
+void fbpinstatelerioku()
+{
+
+                    dosyaokufbyol();
+                    String getpath="/" + YOL + "/pins/"+esphostname+"pin";
+                          //  int str_len = getpath.length() + 1; 
+                          //  char getp[str_len];
+                          //  getpath.toCharArray(getp, str_len);
+
+                    
+//                    String resul=Firebase.RTDB.getString(&fbdo, getpath) ? fbdo.to<const char *>() : fbdo.errorReason().c_str();
+                      String resul = Database.get<String>(aClient, getpath);
+//Serial.println(getpath +"  ");
+Serial.println("Get str. resul");
+                          if(resul=="path not exist")
+                          {
+                            fbdataguncelle();
+                          }
+                          else
+                          {
+                            String reslt=resul;
+                                degisenler="";
+                                for(int h=0;h<pinsayisi;h++)
+                                {
+                                  if(reslt==".")break;
+                                  String pnm=reslt.substring(0,reslt.indexOf(":"));
+                                  String pns=reslt.substring(reslt.indexOf(":")+1,reslt.indexOf(","));
+                                  reslt = reslt.substring(reslt.indexOf(",")+1,reslt.length());
+                                  //Serial.println(pnm+" "+pns);
+                                  
+                                for(int hh=0;hh<pinsayisi;hh++)
+                                    {
+                                      if(pnm == pinname[hh])
+                                      { 
+
+                                        if(pinmode[hh]!="INP"){
+                                            if(psco==false && pns!=fbPinState[hh])
+                                            {
+                                              
+                                              PinState[hh] =pns;
+                                              fbPinState[hh] = pns;
+                                              //pindurumrecyap=true;
+                                              degisenler += pinname[hh] + ":" + PinState[hh]+">" + pinlabel[hh] + ",";
+                                            }
+
+                                        }
+
+                                        break;
+                                      }
+                                    }
+
+                                }
+                                //if(pindurumrecyap==true)dosyayazpindurum();
+                                Serial.print("psco:");
+                                Serial.print(psco);
+                                Serial.print("  psci:");
+                                Serial.println(psci);
+
+                              if(psco==true || psci==true){fbpinstateleriyaz();}
+                            //String resul=Firebase.RTDB.setString(&fbdo, setpath, dats) ? "ok" : fbdo.errorReason().c_str();
+                            //Database.set<string_t>(aClient, setpath, string_t(dats));
+                            //Serial.println("Set string... " + setpath + " " + resul);
+
+                          }
+}
+
+
+
+void fbpinayarlarioku()
+{
+                            // pin ayarlarını yükle
+                            String getpath="/" + YOL + "/pays/" + esphostname + "pay";
+                            Serial.print("Gpath: " + getpath);
+//                            String resul=Firebase.RTDB.getString(&fbdo, getpath) ? fbdo.to<const char *>() : fbdo.errorReason().c_str();
+                            String resul = Database.get<String>(aClient, getpath);
+
+
+                            String dats="";
+                                //for(int h=0;h<pinsayisi;h++)
+                                //{
+                                //  if( pinsatir[h].length()>0) dats+="[" + pinsatir[h] + "]";
+                                //}
+
+                                dats="[" + pinayar;
+                                dats.replace("\n","][");
+                                dats.replace("[]","");
+                                if(dats.length()>1)dats=dats.substring(0,dats.length()-1);
+
+                          if(resul.indexOf("path not exist")>-1 || resul=="null")
+                          {
+                            fbpinayarlariyaz();
+                          }else
+                          {
+                            Serial.println(resul);
+                            Serial.println(dats);
+                            if(resul != dats) fbpinayarlariyaz();
+                            else{Serial.println("aynı");}
+                          }
+
+
+
+
+                            /////////////////////////////
+}
+
+void fbpinayarlariyaz()
+{
+                            // pin ayarlarını yükle
+                            String setpath="/" + YOL + "/pays/" + esphostname + "pay";
+                            String dats="";
+                                //for(int h=0;h<pinsayisi;h++)
+                                //{
+                                //  if( pinsatir[h].length()>0) dats+="[" + pinsatir[h] + "]";
+                                //}
+                                dats="[" + pinayar;
+                                dats.replace("\n","][");
+                                dats.replace("[]","");
+                                if(dats.length()>1)dats=dats.substring(0,dats.length()-1);
+
+                            //Serial.print("Spath: " + setpath);Serial.println(dats);
+//                            String resul=Firebase.RTDB.setString(&fbdo, setpath, dats) ? "ok" : fbdo.errorReason().c_str();
+                           Database.set<string_t>(aClient, setpath, string_t(dats));
+
+                            /////////////////////////////
+}
+
+void fbpinstateleriyaz()
+{
+                            fbsayacguncelle();
+                            String setpath="/" + YOL + "/pins/" + esphostname+ "pin";
+                            String dats="";
+                                for(int h=0;h<pinsayisi;h++)
+                                {
+                                  //if(pinname[h].indexOf("|")>-1)break;
+                                  //Serial.print(pinname[h] + " ");Serial.println(PinState[h]);
+                                  if(PinState[h]=="")PinState[h]="0";
+                                  if(pinname[h].length()>0) dats+=pinname[h] + ":" + PinState[h] + ",";
+                                }
+                                
+//                            String resul=Firebase.RTDB.setString(&fbdo, setpath, dats) ? "ok" : fbdo.errorReason().c_str();
+                              Database.set<string_t>(aClient, setpath, string_t(dats));
+
+                                    for(int hh=0;hh<pinsayisi;hh++)
+                                    {
+                                              //ePinState[hh] = PinState[hh];
+                                              fbPinState[hh] = PinState[hh];
+                                    }
+                            psco=false;
+                            psci=false;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//          if(sayPtakipicin>100){
+//            if(programdata.length()>1)Programtakip(programdata);
+//            sayPtakipicin=0;
+//          }
+//          sayPtakipicin+=1;
+
+/*
+   // save
+  DynamicJsonDocument saveDoc(1024);
+  saveDoc["name"] = "Device 1";
+  saveDoc["temperature"] = 30.00;
+  saveDoc["location"][0] = 48.756080;
+  saveDoc["location"][1] = 2.302038;
+  String saveJSONData;
+  serializeJson(saveDoc, saveJSONData);
+  int saveResponseCode = firebaseRealtime.save("devices", "1", saveJSONData);
+  Serial.println("\nSave - response code: " + String(saveResponseCode));
+  saveDoc.clear();
+
+  // update
+  DynamicJsonDocument updateDoc(1024);
+  updateDoc["temperature"] = 35.00;
+  String updateJSONData;
+  serializeJson(updateDoc, updateJSONData);
+  int updateResponseCode = firebaseRealtime.save("devices", "1", updateJSONData, true);
+  Serial.println("\nUpdate - response code: " + String(updateResponseCode));
+  updateDoc.clear();
+
+  // fetch
+  DynamicJsonDocument fetchDoc(1024);
+  int fetchResponseCode = firebaseRealtime.fetch("devices", "1", fetchDoc);
+  float temp = fetchDoc["temperature"];
+  String name = fetchDoc["name"];
+  float lat = fetchDoc["location"][0];
+  float lon = fetchDoc["location"][1];
+  Serial.println("\nFetch - response code: " + String(fetchResponseCode));
+  Serial.println("Name: " + name + ", Temp: " + String(temp) + ", Lat: " + String(lat) + ", Lon: " + String(lon));
+  fetchDoc.clear();
+*/
---
+++ LOLiN_MQTTprogram_v4/updatepinsayac.ino
@@ -0,0 +1,216 @@
+float t;
+int h;
+void updatehcsr()
+{
+          for(int x=0;x<pinsayisi;x++){
+            
+                        if(pinsignaltype[x]=="HCE"){
+                          hcsr04loop(x);
+                        }
+          }
+}
+
+
+void updateinput()
+{ degisenler="";
+        for(int x=0;x<pinsayisi;x++){
+
+                       if(pinmode[x]=="INP")
+                       {
+                        if(pinsignaltype[x]=="ANG"){
+                        pinMode(Pin[x], INPUT);
+                        //delay(5);
+                        PinState[x] = analogRead(Pin[x]);
+
+                        }
+
+                        if(pinsignaltype[x]=="DIG"){
+                        pinMode(Pin[x], INPUT);
+                        //delay(5);
+                        PinState[x] = digitalRead(Pin[x]);
+
+                        }
+
+                        if(pinsignaltype[x]=="HCE"){
+                          hcsr04loop(x);
+                        }
+                        
+                      if(pinsignaltype[x].indexOf("DHT")==0)
+                      { dhtsayac+=1; 
+                         if(dhtsayac>5){
+
+                          Serial.println("DHTDONGUSU"); 
+                              dhtsayac=0;
+                              uint8_t DHT_TYPE;
+                              if(pinsignaltype[x]=="DHT1") {DHT_TYPE= DHT11;}
+                              if(pinsignaltype[x]=="DHT2") {DHT_TYPE= DHT12;}
+                              if(pinsignaltype[x]=="DHT3") {DHT_TYPE= DHT21;}
+                              if(pinsignaltype[x]=="DHT4") {DHT_TYPE= DHT22;}
+                              DHT DHTA(Pin[x], DHT_TYPE);
+                              //DHT
+
+                              t = DHTA.readTemperature();  // boş iken  celcius // f = DHTA.readTemperature(true); fahrenayt 
+                              delay(50);
+
+                              h = DHTA.readHumidity();
+                              delay(50);
+
+                              // Check if any reads failed and exit early (to try again).
+                              if (isnan(h) || isnan(t))  // || isnan(f)
+                              {
+                                  erlog = "DHT sensor okunamadı";
+                              } else 
+                              {
+
+                                    if(h<100)
+                                    {
+
+                                      // sıcaklık hatası düzelt
+                                      float tvehata=t+pinval[x].toFloat();
+                                      String ts=(String)tvehata;
+
+                                      // nem hatası düzelt
+                                      int hvehata=h+pinmaxvalue[x].toInt();
+                                      String hs=(String)hvehata;
+
+                                        // kısa veri gönder
+                                      if(pinminvalue[x]=="0") ts = ts.substring(0,ts.length()-1);
+                                      if(pinminvalue[x]=="0") hs = hs.substring(0,hs.length()-1);
+
+                                      if(pinminvalue[x]=="1") ts = ts.substring(0,ts.length()-3);
+                                      if(pinminvalue[x]=="1") hs = hs.substring(0,hs.length()-1);
+
+                                        PinState[x] ="t" + ts + "h" + hs;
+                                        Serial.print(("Nem: "));
+                                        Serial.print(hs);
+                                        Serial.print(("0%  Sıcaklık: "));
+                                        Serial.print(ts);
+                                        Serial.print((" C"));
+                                        Serial.println(".");
+                                    }
+                              }
+                                                        Serial.println("DHTDONGUSU-çıkış"); 
+                        }
+
+                      }
+
+          if(ePinState[x]!=PinState[x])
+          {
+             if (pinsignaltype[x].indexOf("DH")>-1)
+                      {
+                          psci=true;
+                          
+                          ePinState[x] = PinState[x];
+                              degisenler += pinname[x] + ":" + PinState[x] + "|" + pinlabel[x] + ",";
+                      }
+
+
+
+                         if(pinsignaltype[x]=="HCE" && PinState[x].toInt()>2)
+                         {
+                          psci=true;
+                          ePinState[x] = PinState[x];
+                              degisenler += pinname[x] + ":" + PinState[x] + "|" + pinlabel[x] + ",";
+                         } else
+                              if(pinsignaltype[x]=="ANG")
+                              {
+
+                                int epinstate = ePinState[x].toInt();
+                                int pinstate  =  PinState[x].toInt();
+
+
+                                if(abs(epinstate-pinstate)>10){
+                                  //Serial.println("Farklı olan e: " + pinname[x] + " " +ePinState[x] + " >> " + PinState[x]);
+                                  psci=true;
+                                  ePinState[x] = PinState[x];
+                                  degisenler += pinname[x] + ":" + PinState[x] + "|" + pinlabel[x] + ",";
+                                }
+
+                              } else
+                              {
+                              psci=true;
+                              ePinState[x] = PinState[x];
+                              degisenler += pinname[x] + ":" + PinState[x] + "|" + pinlabel[x] + ",";
+                              }
+
+
+
+
+
+          }
+
+                       }
+        }
+}
+
+
+void updatefbvirtual()
+{
+          if(authdebug==10){
+                for (int fbg=0;fbg<11;fbg++)
+                {
+                  if(fbc[fbg].length()>0)
+                  {
+                    //Serial.println("fbcyol: " + fbcyol[fbg]);
+                    //Serial.println("fbtd[fbg] " + fbtd[fbg] + "     efbtd[fbg]" +efbtd[fbg] );
+                    if(fbtd[fbg]!=efbtd[fbg])
+                    {
+                      fbbaskacihazagonder(fbcyol[fbg], fbtd[fbg], fbg);
+                    }
+                  }
+                }
+        }
+}
+
+
+void updateoutput()
+{
+       
+
+
+        for(int x=0;x<pinsayisi;x++){
+                  if(pinmode[x]=="OUT"){
+                        if(pinsignaltype[x]=="PWM"){
+                          int PWMdegerint=PinState[x].toInt();
+                          Outpwm(pinname[x], PWMdegerint);
+                        }
+                        if(pinsignaltype[x]=="DIG"){
+                                    if(PinState[x]=="0.00" || PinState[x]=="0" || PinState[x] == "LOW" || PinState[x] =="OFF" || PinState[x]=="")  {
+                                      if(high_low_invert==false)digitalWrite(Pin[x], LOW ); else  digitalWrite(Pin[x], HIGH);
+                                    }
+                                      else
+                                    if(PinState[x]=="1.00" || PinState[x]=="1" || PinState[x] == "HIGH" || PinState[x] =="ON")  
+                                    {
+                                      if(high_low_invert==false)digitalWrite(Pin[x], HIGH); else  digitalWrite(Pin[x], LOW);
+                                    }
+                        }
+
+
+
+          if(ePinState[x]!=PinState[x])
+          {
+              //Serial.println("Farklı olan e: " + pinname[x] + " " +ePinState[x] + " >> " + PinState[x]);
+
+                       if(pinmode[x]=="OUT")
+                       {
+                         psco=true;
+                         //pindurumrecyap=true;
+                         ePinState[x] = PinState[x];
+                         degisenler += pinname[x] + ":" + PinState[x]+ "|" + pinlabel[x] + ",";
+                       }
+          }
+
+        }
+/*
+              if(pindurumrecyap==true)
+              {
+                dosyayazpindurum();
+              }
+*/
+        }
+      if(degisenler!="" && degisenler!= edegisenler){
+        if(htServerip.length()>3)sendserver(htServerip, degisenler);
+      }
+        yield();
+
+}
