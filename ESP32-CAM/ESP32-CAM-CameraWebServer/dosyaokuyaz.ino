c78b8db6af7e8dd01ee8acc5ca3158b5af4490da
Murat Hoca <mbshurricane@gmail.com>
2026-01-15 21:11:02 +0300

    espcam
    esp32-cam

---
+++ ESP32-CAM/ESP32-CAM-CameraWebServer/dosyaokuyaz.ino
@@ -0,0 +1,132 @@
+/*
+void dosyaOkuSERVERusers(){
+
+  dosya.close();
+  dosya = LittleFS.open("/SERVERusers.txt", "r");
+  if (dosya) {
+          String geciciSERVERusers = dosya.readString();
+          geciciSERVERusers = geciciSERVERusers.substring(0, geciciSERVERusers.length() - 1);
+          SERVERusers = geciciSERVERusers;
+          Serial.println("geciciSERVERusers v ");
+          Serial.println(geciciSERVERusers);
+          Serial.print("SERVERusers=================>");
+          Serial.print(SERVERusers);
+          Serial.println("<");
+          dosya.close();
+        SERVERkullanici=SERVERusers;
+
+
+          for (int j=0;j<SERVERkullanici.length();j++){
+              Kullaniciadi[j] = SERVERkullanici.substring(0,SERVERkullanici.indexOf("|"));
+              SERVERkullanici= SERVERkullanici.substring(SERVERkullanici.indexOf("|")+1,SERVERkullanici.length());
+
+              Kullanicisifresi[j] = SERVERkullanici.substring(0,SERVERkullanici.indexOf("\n"));
+              SERVERkullanici= SERVERkullanici.substring(SERVERkullanici.indexOf("\n")+1,SERVERkullanici.length());
+
+              if(SERVERkullanici.length()<1) break;
+
+              Serial.println(Kullaniciadi[j]+"|"+ Kullanicisifresi[j]);
+
+          }
+      }
+}
+
+
+void dosyaYazSERVERusers(){
+  dosya.close();
+  LittleFS.remove("/SERVERusers.txt");
+  Serial.println(SERVERusers);
+  if(SERVERusers.indexOf(SERVERusers.substring(SERVERusers.length()- 5, 5)),"%0"<0)SERVERusers;
+  dosya = LittleFS.open("/SERVERusers.txt", "w+");
+  SERVERusers=Karakterduzeltfunc(SERVERusers);
+  dosya.println(SERVERusers);
+  dosya.close();
+  dosyaOkuSERVERusers();
+
+  //ESP.reset();
+}
+*/
+
+void dosyaokussidpass() {
+
+  //if (digitalRead(FactoryDefault) == HIGH) {
+    // cleareprom();
+  //}
+
+  dosya = LittleFS.open("/ssidpass.txt", "r");
+  if (dosya) {
+    // dosya başarı ile açıldı;
+    String ssidoc = dosya.readStringUntil('\n');
+    ssid = ssidoc.substring(0, ssidoc.length() - 1);
+    //Serial.println(ssid);
+
+    String passoc = dosya.readStringUntil('\n');
+    pass = passoc.substring(0, passoc.length() - 1);
+    //Serial.println(pass);
+
+    dosya.close();
+    //Serial.println("read bitti");
+  }else{ssid="Zyxel";pass="bebedede14";}
+  if(ssid.length()<2){ssid="Zyxel";pass="bebedede14";}
+}
+
+
+
+void dosyayazssidpass()
+{
+                  reConnectsayac=millis();
+                dosya.close();
+                LittleFS.remove("/ssidpass.txt");
+                dosya = LittleFS.open("/ssidpass.txt", "w+");
+                if (dosya) {
+                  dosya.println(ssid);
+                  dosya.println(pass);
+                  dosya.close();
+                  Serial.println("Write bitti");
+                }
+                dosya.close();
+
+}
+
+
+String myssidyazilimi = "";
+void dosyayazmyssidname() {
+  dosya.close();
+  LittleFS.remove("/myssidname.txt");
+  dosya = LittleFS.open("/myssidname.txt", "w+");
+  if(dosya){
+  dosya.println(myssidyazilimi);
+  Serial.println(myssidyazilimi);
+  dosya.close();
+  dosyaokumyssidname();
+  WiFi.hostname(esphostname);
+  WiFi.softAP(esphostname, "12345678");
+  }
+//  fbchzkaydial();
+}
+
+
+
+void dosyaokumyssidname() {
+  //   я
+  int yazili = 0;
+  myssidyazilimi = "";
+  dosya.close();
+  dosya = LittleFS.open("/myssidname.txt", "r");
+
+  if (dosya) {
+    String gecicimyssidname = dosya.readStringUntil('\n');
+    gecicimyssidname = gecicimyssidname.substring(0, gecicimyssidname.length() - 1);
+    Serial.println(gecicimyssidname);
+    myssidyazilimi = gecicimyssidname;
+    yazili = gecicimyssidname.length();
+    dosya.close();
+    if (yazili < 1) { myssidyazilimi = "v4"; }
+
+    esphostname = esphostnameOnek + "-" + myssidyazilimi;
+  
+  }
+}
+
+
+
---
+++ ESP32-CAM/ESP32-CAM-CameraWebServer/httpcl.ino
@@ -0,0 +1,245 @@
+int xcurrentTime=0;
+int timeoutTime=5000;
+
+void htpcl() {
+  WiFiClient xilent = htserver.available();
+  header = "";
+  if (xilent) {
+
+    // If a new xilent connects,
+
+    String currentLine = "";                             // make a String to hold incoming data from the xilent
+    xcurrentTime = 0;
+    while (xilent.connected() && xcurrentTime <= timeoutTime) {  // loop while the xilent's connected
+      xcurrentTime += 1;
+      if (xilent.available()) {  // if there's bytes to read from the xilent,
+
+        for (int q = 0; q < 2000; q++) {
+          char c = xilent.read();  // read a byte, then
+
+          //Serial.print((int)c);
+          if (int(c) == 255) break;
+          Serial.print(c);  // print it out the serial monitor
+          header += c;
+        }
+          if(header.indexOf("\n")<1)return;
+
+
+          // Read the first line of HTTP request
+          // First line of HTTP request looks like "GET /path HTTP/1.1"
+          // Retrieve the "/path" part by finding the spaces
+
+          header = header.substring(0, header.indexOf("\n"));
+
+          Serial.println(header);
+          Serial.println("");
+    if(header.indexOf("/favicon") > -1)return;
+
+    xilent.println("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>ESP32: Selam");
+    xilent.println("<head><meta charset=\"UTF-8\"><style>body {background-color: #999999;} </style>");
+                        xilent.println("<meta name=\"viewport\" xo=\"width=device-width, initial-scale=1\">");
+                        //xilent.println("<META charset=iso-8859-9\">";
+                        xilent.println("<link rel=\"icon\" href=\"data:,\">");
+                        xilent.println("</head>");
+                        xilent.println("<body>");
+                        xilent.println("<font size=\"3\" color=\"#FF0000\">" + esphostname + "</font>");
+                        xilent.println("<br><table style=\"border:5px solid black;width:500px\"><tr>");
+                        //xilent.println("<td style=\"border:1px solid black;width:250p; align:center; \">");
+                        //xilent.println("<p><form action=\"/Menu0\" method=\"POST\"><input type=\"submit\" value=\"Anasayfa\"></form></p></td>");
+                        xilent.println("<td style=\"border:1px solid black;width:250p; align:center; \">");
+                        xilent.println("<p><form action=\"/\" method=\"POST\"><input type=\"submit\" value=\"Durum\"></form></p></td>");
+                        //xilent.println("<td style=\"border:1px solid black;width:250px\">");
+                        //xilent.println("<p><form action=\"/pnayar\" method=\"POST\"><input type=\"submit\" value=\"Pin ayar\"></form></p></td>");
+                        xilent.println("<td style=\"border:1px solid black;width:250px;\">");
+                        xilent.println("<p><form action=\"/ssidset\" method=\"POST\"><input type=\"submit\" value=\"Bağlantı ayar\"></form></p></td>");
+                        xilent.println("<td style=\"border:1px solid black;width:250px\">");
+                        xilent.println("<p><form action=\"/myssidnameayar\" method=\"POST\"><input type=\"submit\" value=\"Cihaz adı ayar\"></form></p></td>");
+                        //xilent.println("<td style=\"border:1px solid black;width:250px\">");
+                        //xilent.println("<p><form action=\"/serveripayar\" method=\"POST\"><input type=\"submit\" value=\"Server ip ayar\"></form></p></td>");
+                        
+                        //xilent.println("<td style=\"border:1px solid black;width:250px\">");
+                        //xilent.println("<p><form action=\"/mqttipayar\" method=\"POST\"><input type=\"submit\" value=\"MQTT ayar\"></form></p></td>");
+                        //xilent.println("<td style=\"border:1px solid black;width:250px\">");
+                        //xilent.println("<p><form action=\"/firebaseset\" method=\"POST\"><input type=\"submit\" value=\"Firebase ayar\"></form></p></td>");
+
+                        xilent.println("</tr></table>");
+
+
+
+
+    // ssidpass başı /////////////////////
+   if (header.indexOf("/ssidset") > -1) {
+    xilent.println("Bağlantı ayar bölümü");
+    xilent.println("<br><br>");
+    xilent.println("        BAĞLANTI YAPILACAK CİHAZ BİLGİLERİ GİRİŞİ");
+    xilent.println("<hr style=\"height:6px;border-width:1;color:black;background-color:black\">");
+    xilent.println("<br>");
+
+    // ssidpass başı /////////////////////
+    if (header.indexOf("/ssidset?") > -1) {
+      //   "/ssidset?ssid=Zyxel&pass=bebedede14"
+      // yazma kısmı
+      Serial.println("ssidset yazma kısmı");
+      if (header.indexOf("ssid=") > -1) {
+        ssid = header.substring((header.indexOf("ssid=") + 5), header.indexOf("&"));
+        Serial.println(ssid);
+      }
+      if (header.indexOf("&pass=") > -1) {
+        pass = header.substring((header.indexOf("&pass=") + 6), header.indexOf(" HTTP/"));
+        Serial.println(pass);
+        dosyayazssidpass();
+        ////client.abort();;
+        ESP.restart();
+      }
+    }
+
+    IPAddress gip = WiFi.gatewayIP();
+    String gipStr = String(gip[0]) + '.' + String(gip[1]) + '.' + String(gip[2]) + '.' + String(gip[3]);
+    IPAddress dns = WiFi.dnsIP();
+    String dnsStr = String(gip[0]) + '.' + String(gip[1]) + '.' + String(gip[2]) + '.' + String(gip[3]);
+    IPAddress ip = WiFi.softAPIP();
+    String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
+
+    xilent.println("<table style=\"border:2px solid green;width:500px\"><tr><td style=\"border:1px solid black;width:250px\">");
+    xilent.println("Bilgiler");
+    xilent.println("<br><br>");
+    if(WiFi.status()==WL_CONNECTED)xilent.println("Bağlanılan wifi cihazı:" + ssid);
+    else xilent.println("Herhangi bir wifi cihaza bağlı değilim");
+    xilent.println("<br>");
+    xilent.println("Local IP : ");
+    IPAddress lip = WiFi.localIP();
+    String lipStr = String(lip[0]) + '.' + String(lip[1]) + '.' + String(lip[2]) + '.' + String(lip[3]);
+    xilent.println(lipStr);
+    xilent.println("<br>");
+    xilent.println("Local Gateway : ");
+    xilent.println(gipStr);
+    xilent.println("<br>");
+    xilent.println("Access IP : ");
+    xilent.println(ipStr);
+    xilent.println("</td><td style=\"border:1px solid black;width:250px\">");
+    xilent.println("<form action=\"/ssidsetscanwifi\" method=\"POST\"><input type=\"submit\" value=\"Wifi tara\"></form>");
+    if (header.indexOf("scanwifi HTTP") > -1) {
+      rescanwifi = 1;
+    }
+              xilent.println("<br>Wifi tara tıkladıktan sonra 10 sn bekle<br>");
+              xilent.println("<br>Sonra tarama sonucu butonuna tıkla<br>");
+              xilent.println("<form action=\"/ssidset\" method=\"POST\"><input type=\"submit\" value=\"Tarama sonucu\"></form>");
+              xilent.println("</td></tr></table>");
+
+
+              xilent.println("<p>");
+              xilent.println("<table style=\"border:2px solid black;width:500px\"><tr><td style=\"border:1px solid black;width:500px\">");
+              xilent.println("Tarama sonucunda bulunabilen Wifi cihazları listesi</td><tr><td>");
+              xilent.println("<br>");
+              xilent.println(st);
+              xilent.println("</td><tr></table><br>");
+              xilent.println("Bağlanılacak Modemin Wifi ismi (SSID) ve Şifresi (PASSWORD) giriş bölümü.");
+              xilent.println("            Kaydettikten sonra cihaza reset gerekli.");
+              xilent.println("<br><form action=\"/ssidset\" method=\"get\"><label>SSID: </label><input name='ssid' length=32><label>PASS: </label><input name='pass' length=32><input type='submit'value='Kaydet'></form>");
+// ssidpass sonu /////////////////////
+   }
+
+// myssidname başı //////////////////
+             if (header.indexOf("/myssidnameayar") > -1) {  
+              xilent.println("Cihaz adı ayar bölümü");
+              xilent.println("<br><br>");
+              xilent.println("        BENİM ADIM BİLGİ GİRİŞİ");
+              xilent.println("<hr style=\"height:6px;border-width:1;color:black;background-color:black\">");
+              xilent.println("<br><br>");
+
+// myssidname başı //////////////////
+            if (header.indexOf("/myssidnameayar?is=") > -1) {
+              myssidyazilimi = header.substring((header.indexOf("?is=") + 4), header.indexOf(" HTTP/"));
+              Serial.println("buradan geçtim");
+              dosyayazmyssidname();
+            }
+
+              xilent.println("    Benim Wifi ismim giriş bölümüdür.<br>");
+              xilent.println("    <form method='get' action='myssidnameayar'><label>Benim wifi ismim (SSID) : " + esphostnameOnek + "-</label><input name='is' length=32 value='");
+              xilent.println(myssidyazilimi);
+              //xilent.println("><br>");
+              //xilent.println("<label>Baglanilacak SERVER adressi: </label><input name='mq' length=32 value=");
+//              xo += SERVERip;
+              xilent.println("'>    <input type='submit'>");
+              xilent.println("</form>");
+
+             }
+
+// myssidname sonu ///////////////////////
+
+
+            if (header.indexOf("/ HTTP/1.1") > -1) {
+
+              //Serial.println(Menu);
+              //Serial.println(header);
+              //Serial.println(xo);
+              IPAddress lip = WiFi.localIP();
+              String lipStr = String(lip[0]) + '.' + String(lip[1]) + '.' + String(lip[2]) + '.' + String(lip[3]);
+              IPAddress gip = WiFi.gatewayIP();
+              String gipStr = String(gip[0]) + '.' + String(gip[1]) + '.' + String(gip[2]) + '.' + String(gip[3]);
+              IPAddress dns = WiFi.dnsIP();
+              String dnsStr = String(gip[0]) + '.' + String(gip[1]) + '.' + String(gip[2]) + '.' + String(gip[3]);
+              IPAddress ip = WiFi.softAPIP();
+              String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
+
+
+              //xilent.println("<p>");
+              xilent.println("Durum sayfası");
+              //if(Firebase.ready()) xilent.println("<font size=\"3\" color=\"#FF0000\">Dikkat Firebase çalışıyor. <br> Ayarlar yapılıp kaydedilirken ESP ye bağlı tüm cihazların gücünü kapatın.<br> Kayıttan sonra Reset anında pinlere İstek dışında enerji yollanabilir.<br> cihazlar istek dışı çalışabilir.<br></font>");
+              xilent.println("<br><br>");
+              xilent.println("    Local IP : ");
+              xilent.println(lipStr);
+              xilent.println("<br>");
+
+              xilent.println("    Local Gateway : ");
+              xilent.println(gipStr);
+              xilent.println("<br>");
+
+              xilent.println("    Access IP : ");
+              xilent.println(ipStr);
+
+              xilent.println("<br>");
+              xilent.println("<br>    Wifi ismim       :" + esphostname);
+              if (WiFi.status() == WL_CONNECTED) xilent.println("<br>    Bağlanılan Wifi  :" + ssid + "  Sinyal gücü : " + WiFi.RSSI());
+              else xilent.println("<br>    Kayıtlı Wifi  :" + ssid + "  bağlanılamadı.");
+
+              xilent.println("<br>    Kamera durum: ");
+              //xilent.printf("Camera init failed with error 0x%x", err);
+
+              if (err != ESP_OK) {
+                  xilent.println("Camera init failed with error 0x% x");
+                  xilent.println(err);
+                } else
+                { xilent.printf("Camera init OK ");// camerr="Camera init OK " + err;
+                  xilent.println(err);
+                }
+              } 
+
+
+
+              xilent.println("<br><br>");
+              xilent.println("<hr style=\"height:6px;border-width:1;color:black;background-color:black\">");
+              xilent.println("<br>");
+
+
+              xilent.println("    Reset bölümüdür.<br>");
+              xilent.println("    <form method='post' action='reset'><label>        Reset</label>");
+              xilent.println("   <input type='submit'>");
+              xilent.println("</form>");
+
+              xilent.println("<br>");
+              xilent.println("<hr style=\"height:6px;border-width:1;color:black;background-color:black\">");
+              xilent.println("<br>");
+
+
+    xilent.println("</body></html>\r\n\r\n");
+
+
+          xilent.stop();
+          Serial.println("Client disconnected.");
+          Serial.println("");
+          if(header.indexOf("/reset")>-1)ESP.restart();
+      }
+    }
+  }
+}
